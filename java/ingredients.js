14ccae8767fda044c6bbd979e09f170eb26d2b30
kavi678 <kavipatel111.kp@gmail.com>
2019-05-22 18:22:24 +0930

    jgjcfg

--- java/ingredients.js
+++ java/ingredients.js
@@ -20,8 +20,8 @@ function myFunction() {
 var Day1Array = [];
 // following init fuction will always run when the windw is open. it will take all the items from the local storage and give value to post on a page
 function init() {
-  if (localStorage.Add2) {
-      Day1Array = JSON.parse(localStorage.Add2);
+  if (localStorage.Add1) {
+      Day1Array = JSON.parse(localStorage.Add1);
       for (var i = 0; i < Day1Array.length; i++) {
           prepareTableCell(Day1Array[i].exer1, Day1Array[i].mus1, Day1Array[i].se, Day1Array[i].re)
 
@@ -41,7 +41,7 @@ function add1() {
     var r1 = document.getElementById("rep1").value;
     var stuObj = {exer1: ex1, mus1: mu1, se: s1, re: r1};
     Day1Array.push(stuObj);
-    localStorage.Add2 = JSON.stringify(Day1Array);
+    localStorage.Add1 = JSON.stringify(Day1Array);
 
     prepareTableCell(ex1, mu1, s1, r1);
 
@@ -68,7 +68,7 @@ function prepareTableCell(ex1, mu1, s1, r1) {
 // dell function will delete that key from the local storage. Day1Array will be removes from it. when you add again any item it will create a new Day1Array list.
 function del1() {
 
-    localStorage.removeItem("Add2");
+    localStorage.removeItem("Add1");
 
 }
 function goBack() {
@@ -80,4 +80,14 @@ function openForm() {
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+}
+function openForm1() {
+  document.getElementById("cc11").style.display = "none";
+  document.getElementById("hideing").style.display="block";
+  document.getElementById("hidesen").style.display="none";
+}
+function closeForm1() {
+  document.getElementById("cc11").style.display = "none";
+  document.getElementById("hideing").style.display="block";
+  document.getElementById("hidesen").style.display="none";
 }
